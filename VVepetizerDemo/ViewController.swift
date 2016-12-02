8cfb3ee5c5c62fd505224ebf7c229aa936ad1608
jydemo <jydemo@qq.com>
2016-12-02 15:48:52 +0800

    Initial Commit

---
+++ VVepetizerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VVepetizerDemo
+//
+//  Created by atom on 2016/12/2.
+//  Copyright © 2016年 atom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
